212eadad01b09797c4a7416c9e23c8341ad8750b
disconnect-ed <ruslan199812@gmail.com>
2020-10-05 10:16:16 +0300

    added sass, added ant design, added types to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, FC} from 'react';
+import '~antd/dist/antd.css';
 import './App.css';
 import {getDynamicData} from "./redux/actions/rates-action";
 import {useDispatch} from "react-redux";
 
 
-function App() {
+const App: FC = () => {
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getDynamicData(145, '2020-10-1', '2020-10-4'))
